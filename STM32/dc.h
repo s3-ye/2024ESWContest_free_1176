97d3edf2955c3da1365859568fb0ccab49280c84
s3-ye <yeseo0925@tukorea.ac.kr>
2025-09-03 23:52:03 +0900

    Create dc.h

---
+++ STM32/dc.h
@@ -0,0 +1,13 @@
+#ifndef INC_DC_H_
+#define INC_DC_H_
+
+#include "main.h"
+
+#define IN1_PIN GPIO_PIN_0
+#define IN2_PIN GPIO_PIN_1
+
+void MOTOR_ON(void);
+void MOTOR_OFF(void);
+
+
+#endif /* INC_DC_H_ */
